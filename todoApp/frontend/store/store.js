b65fa8ec8fb129f58368f14901c83bba110600b1
gamestar <rinayumiho@gmail.com>
2021-08-27 04:06:42 +0800

    Starting thunk

--- todoApp/frontend/store/store.js
+++ todoApp/frontend/store/store.js
@@ -1,8 +1,9 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import rootReducer from '../reducers/root_reducer';
+import thunk from '../middleware/thunk';
 
 const configureStore = (preloadedState = {}) => {
-  const store = createStore(rootReducer, preloadedState);
+  const store = createStore(rootReducer, preloadedState, applyMiddleware(thunk));
   store.subscribe(() => {
     localStorage.state = JSON.stringify(store.getState());
   });
--- todoApp/frontend/todo_redux.jsx
+++ todoApp/frontend/todo_redux.jsx
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom';
 import configureStore from './store/store';
 
 import Root from './components/root';
+import { fetchTodos } from './util/todo_api_util';
 
 document.addEventListener('DOMContentLoaded', () => {
   const preloadedState = localStorage.state ?
     JSON.parse(localStorage.state) : {};
   const store = configureStore(preloadedState);
 
-  const root = document.getElementById('content');
+  const root = document.getElementById('root');
   ReactDOM.render(<Root store={store} />, root);
+  window.fetchTodos = fetchTodos;
 });
